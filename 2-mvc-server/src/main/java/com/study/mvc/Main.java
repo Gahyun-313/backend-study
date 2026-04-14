cf438700bc0033eadceaf8518a25095313b8ee39
Gahyun-313 <gus20313@gmail.com>
2026-04-14 23:37:20 +0900

    chore(2-mvc): 서브모듈 및 디렉토리 구조 생성
    - settings.gradle에 2-mvc-server 추가
    - 레이어별 패키지 구조 생성
      (controller/service/repository/domain/dto/exception/router/handler/server/util)
    - 테스트 디렉토리 구조 포함 (service/repository)

---
+++ 2-mvc-server/src/main/java/com/study/mvc/Main.java
@@ -0,0 +1,8 @@
+package com.study.mvc;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("MVC 서버 준비 중...");
+        // Commit 5에서 실제 서버 실행 코드 추가 예정
+    }
+}
